fe5ba9e3d34733889e6b8d7996e682ebac0010b6
shawnlawyer <shawnlawyer@gmail.com>
2016-01-31 17:32:08 -0500

    moo

---
+++ src/Application/Models/ORM/Tokens.class.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Sequode\Application\Models\ORM;
+
+use Sequode\Model\Database\SQL\ORM;
+
+class Tokens extends ORM {
+    public $database_connection     =   'accounts_database';
+	public $table 					=	'tokens';
+	public function __construct() {
+		parent::__construct();
+		return true;
+	}
+	public function create(){
+		$sql = "
+			INSERT INTO {$this->table}
+		 	(`id`,`owner_id`,`name`,`token`)
+			VALUES
+		 	(''
+			,0
+			,'New Token'
+            ,".$this->safedSQLData(SQDE_TokenOperations::uniqueHash('SQDETOK'), 'text')."
+            )
+			";
+		$this->database->query($sql);
+		$this->id = $this->database->insertId;
+		return true;
+	}
+}
