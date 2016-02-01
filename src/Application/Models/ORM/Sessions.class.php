ef6ee0bc34402879fb430c9b383e3842d39f77fd
shawnlawyer <shawnlawyer@gmail.com>
2016-02-01 01:52:32 -0500

    moo

--- src/Application/Models/ORM/Sessions.class.php
+++ src/Application/Models/ORM/Sessions.class.php
@@ -4,7 +4,7 @@ namespace Sequode\Application\Models\ORM;
 
 use Sequode\Model\Database\SQL\ORM;
 
-class SQDE_Sessions extends ORM {
+class Sessions extends ORM {
     public $database_connection     =   'sessions_database';
 	public $table                   =	'sessions';
 	public function __construct() {
