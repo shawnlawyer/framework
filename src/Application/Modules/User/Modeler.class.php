ba6ac3e26446162fcbf45344a0a52c87facb8abf
shawnlawyer <shawnlawyer@gmail.com>
2016-02-02 18:15:06 -0500

    moo

--- src/Application/Modules/User/Modeler.class.php
+++ src/Application/Modules/User/Modeler.class.php
@@ -3,5 +3,5 @@
 namespace Sequode\Application\Modules\User;
 
 class Modeler extends \Sequode\Patterns\Modeler {
-	public static $model = Sequode\Application\Models\ORM\Users::class;
+	public static $model = \Sequode\Application\Models\ORM\Users::class;
 }
