c0f05584e71b3c2986445e67848a569ee04158e7
shawnlawyer <shawnlawyer@gmail.com>
2016-02-04 14:14:30 -0500

    moo

--- src/Application/Modules/Account/Operations.class.php
+++ src/Application/Modules/Account/Operations.class.php
@@ -19,7 +19,7 @@ class Operations {
         }
         return $salt . sha1($salt . $text);
     }
-	public static function getModel ersModelCount($_modeler, $_model = null){
+	public static function getModelersModelCount($_modeler, $_model = null){
         $modeler = ModuleRegistry::model(static::$package)->modeler;
         ($_model == null) ? forward_static_call_array(array($modeler,'model'),array()) : forward_static_call_array(array($modeler,'model'),array($_model));
         $_models = new SQDE_Package::$model;
