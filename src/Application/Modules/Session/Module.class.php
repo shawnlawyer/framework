c02b6d89cabf15d20b13d5f523b871af81034de5
shawnlawyer <shawnlawyer@gmail.com>
2016-02-05 21:58:00 -0500

    moo

---
+++ src/Application/Modules/Session/Module.class.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Sequode\Application\Modules\Session;
+
+class Module {
+    public static $package = 'Session'; 
+	public static function model(){
+        $model = (object) null;;
+        $model->context = 'session';
+        $model->collections = Routes\Collections\Collections::class;
+        $model->modeler = 'SQDE_Session';
+        $model->finder = Collections::class;
+        $model->card_objects = Components\Cards::class;
+        $model->form_objects = Components\Forms::class;
+        $model->operations = Operations::class;
+        $model->xhr = (object) null;
+        $model->xhr->operations = Routes\XHR\Operations::class;
+        $model->xhr->forms = Routes\XHR\Forms::class;
+        $model->xhr->cards = Routes\XHR\Cards::class;
+		return $model;
+	}
+}
