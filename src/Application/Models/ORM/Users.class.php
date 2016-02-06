b4c67f99d3a772f39f9184685cd722cc24286321
shawnlawyer <shawnlawyer@gmail.com>
2016-02-06 02:25:26 -0500

    moo

--- src/Application/Models/ORM/Users.class.php
+++ src/Application/Models/ORM/Users.class.php
@@ -25,7 +25,7 @@ class Users extends ORM {
             ,".$this->safedSQLData($email, 'text')."
             ,".time()."
             ,'[]'
-            ,".$this->safedSQLData(sha1(substr(md5(uniqid(rand(), true)), 0, 25));, 'text')."
+            ,".$this->safedSQLData(sha1(substr(md5(uniqid(rand(), true)), 0, 25)), 'text')."
             )";
 		$this->database->query($sql);
 		$this->id = $this->database->insertId;
