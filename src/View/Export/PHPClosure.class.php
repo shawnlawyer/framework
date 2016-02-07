d4d4afc79e3154f52ea41a53a636056408d9ad91
shawnlawyer <shawnlawyer@gmail.com>
2016-02-07 17:25:31 -0500

    moo

--- src/View/Export/PHPClosure.class.php
+++ src/View/Export/PHPClosure.class.php
@@ -4,11 +4,11 @@ namespace Sequode\View\Export;
 class PHPClosure {
     public static function export($variable, $return = false, $delimeter_nl = true ) {
         if ($variable instanceof stdClass) {
-            $result = '(object) '.self::__FUNCTION__(get_object_vars($variable), true, false);
+            $result = '(object) '.self::export(get_object_vars($variable), true, false);
         } else if (is_array($variable)) {
             $array = array ();
             foreach ($variable as $key => $value) {
-                $array[] = var_export($key, true).' => '.self::__FUNCTION__($value, true);
+                $array[] = var_export($key, true).' => '.self::export($value, true);
             }
             if($delimeter_nl == true){
             $result = 'array ('.implode(',
