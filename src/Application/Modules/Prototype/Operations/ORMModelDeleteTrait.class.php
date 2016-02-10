b4f8f8c36e8be09e9dd31b2bc6eb54a80d3b8cde
shawnlawyer <shawnlawyer@gmail.com>
2016-02-09 23:32:13 -0500

    moo

--- src/Application/Modules/Prototype/Operations/ORMModelDeleteTrait.class.php
+++ src/Application/Modules/Prototype/Operations/ORMModelDeleteTrait.class.php
@@ -2,7 +2,7 @@
 
 namespace Sequode\Application\Modules\Prototype\Operations;
 
-class ORMModelDeleteTrait {
+trait ORMModelDeleteTrait {
     public static function delete($_model = null){
         $modeler = ModuleRegistry::model(static::$package)->modeler;
         ($_model == null) ? forward_static_call_array(array($modeler,'model'),array()) : forward_static_call_array(array($modeler,'model'),array($_model));
