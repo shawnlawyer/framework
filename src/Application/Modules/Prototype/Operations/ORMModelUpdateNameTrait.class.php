6fa13622b51457d52813ee12c29a5382aad4b217
shawnlawyer <shawnlawyer@gmail.com>
2016-02-10 00:15:27 -0500

    moo

--- src/Application/Modules/Prototype/Operations/ORMModelUpdateNameTrait.class.php
+++ src/Application/Modules/Prototype/Operations/ORMModelUpdateNameTrait.class.php
@@ -3,10 +3,10 @@
 namespace Sequode\Application\Modules\Prototype\Operations;
 
 trait ORMModelUpdateNameTrait {
-    public static function delete($_model = null){
+    public static function updateName($name, $_model = null){
         $modeler = ModuleRegistry::model(static::$package)->modeler;
         ($_model == null) ? forward_static_call_array(array($modeler,'model'),array()) : forward_static_call_array(array($modeler,'model'),array($_model));
-        $modeler::model()->delete($modeler::model()->id);
+        $modeler::model()->updateField(str_replace(" ","_",$name),'name');
         return $modeler::model();
     }
 }
