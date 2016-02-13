2bf28529f487e72fc8e73d92124232c2446a90be
shawnlawyer <shawnlawyer@gmail.com>
2016-02-12 20:40:05 -0500

    moo

--- src/Application/Modules/Prototype/Operations/User/ORMModelOwnedModuleModelsTrait.class.php
+++ src/Application/Modules/Prototype/Operations/User/ORMModelOwnedModuleModelsTrait.class.php
@@ -2,6 +2,8 @@
 
 namespace Sequode\Application\Modules\Prototype\Operations\User;
 
+use Sequode\Model\Module\Registry as ModuleRegistry;
+
 trait ORMModelOwnedModuleModelsTrait {
     
 	public static function getOwnedModels($module_registry_key, $_model = null, $fields='id'){
@@ -9,13 +11,15 @@ trait ORMModelOwnedModuleModelsTrait {
         $modeler = static::$modeler;
         
         ($_model == null)
-            ? forward_static_call_array(array($modeler,'model'),array())
-            : forward_static_call_array(array($modeler,'model'),array($_model)) ;
+            ? forward_static_call_array(array($modeler, 'model'), array())
+            : forward_static_call_array(array($modeler, 'model'), array($_model)) ;
             
+        $module_modeler = ModuleRegistry($module_registry_key)->modeler;
+        
         $where = array();
         $where[] = array('field'=>'owner_id','operator'=>'=','value'=>$modeler::model()->id);
         
-        $model = new $modeler::$model;
+        $model = new $module_modeler::$model;
         $model->getAll($where, $fields);
         
         return $model;
