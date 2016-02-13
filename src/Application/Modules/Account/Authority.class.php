b7f8f15620827779f5c77f0a6dc7a34c80c2efe1
shawnlawyer <shawnlawyer@gmail.com>
2016-02-12 19:09:11 -0500

    moo

--- src/Application/Modules/Account/Authority.class.php
+++ src/Application/Modules/Account/Authority.class.php
@@ -9,7 +9,7 @@ class Authority {
     public static $modeler = Modeler::class;
     
     public static function isAuthenticated($_model = null){
-        if($_model == null ){ $_model = static::$modeler::model(); }
+        if($_model == null ){ $_model = { static::$modeler }::model(); }
         return (self::isActive($_model) || self::isSystemOwner($_model)) ? true : false;
     }
     public static function isOwner($test_model, $_model = null){
