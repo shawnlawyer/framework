7bb48253e0c60a89d511ae14b56980edc203e95a
shawnlawyer <shawnlawyer@gmail.com>
2016-02-14 22:06:56 -0500

    moo

--- src/Application/Modules/User/Traits/Operations/ORMModelSignOutTrait.class.php
+++ src/Application/Modules/User/Traits/Operations/ORMModelSignOutTrait.class.php
@@ -12,7 +12,9 @@ trait ORMModelSignOutTrait {
         $modeler::model(null);
 		\Sequode\Application\Modules\Session\Modeler::start();
 		\Sequode\Application\Modules\Session\Modeler::set('console','Auth');
+        
         return;
+        
 	}
     
 }
