622f0987f28233d99b23b3a52269c2b3033f802b
shawnlawyer <shawnlawyer@gmail.com>
2016-02-19 19:50:23 -0500

    moo

--- src/Model/Module/Registry.class.php
+++ src/Model/Module/Registry.class.php
@@ -8,22 +8,46 @@ class Registry {
     use StaticStoreTrait;
     
     public static function is($key){
-        self::container('is', $key);
+        
         return self::container('is', $key);
+        
     }
+    
     public static function add($class){
-        self::container('set', $class::$module_registry_key, $class::model());
-        return true;
+        
+        self::container('set', $class::$module_registry_key, $class);
+        
+        return self::container('is', $class::$module_registry_key);
+        
     }
+    
     public static function model($key){
-        return self::container('get', $key);
+        
+        $module = self::container('get', $key);
+        
+        return $module::model();
+        
     }
+    
     public static function models(){
-        return self::container('getAll');
+        
+        $modules = self::container('getAll');
+        
+        $_o = array();
+        foreach($modules as $key => $module){
+            
+            $_o[$key] = $module::model(); 
+            
+        }
+        
+        return $_o;
+        
     }
     
     public static function clear(){
+        
         self::container('clear');
+        
         return true;
     }
 }
