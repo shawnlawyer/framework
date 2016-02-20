8c4eada96fb782598aa049f423a64ca9e86913ed
shawnlawyer <shawnlawyer@gmail.com>
2016-02-19 19:17:34 -0500

    moo

--- src/Model/Module/Registry.class.php
+++ src/Model/Module/Registry.class.php
@@ -1,39 +1,18 @@
 <?php
 namespace Sequode\Model\Module;
 
+use Sequode\Foundation\Traits\StaticStoreTrait;
+
 class Registry {
-    public static function container($mode, $key = null, $value = null) {
-        static $store;
-        if(!is_array($store)){
-            $store = array();
-        }
-        switch($mode){
-            case 'clear':
-                $store = array();
-                break;
-            case 'is':
-                return (array_key_exists($key, $store)) ? true : false ;
-            case 'add':
-                if($key != null){
-                    $store[$key] = $value;
-                }
-                break;
-            case 'get':
-                if(array_key_exists($key, $store)){
-                    return $store[$key];
-                }
-                break;
-            case 'getAll':
-                return $store;
-                break;
-        }
-    }
+    
+    use StaticStoreTrait;
+    
     public static function is($key){
         self::container('is', $key);
         return self::container('is', $key);
     }
-    public static function add($package_class){
-        self::container('add', $package_class::$package, $package_class::model());
+    public static function add($class){
+        self::container('set', $class::class, $class::model());
         return true;
     }
     public static function model($key){
