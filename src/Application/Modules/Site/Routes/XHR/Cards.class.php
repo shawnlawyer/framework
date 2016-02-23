2264232e7065721a6a2b6f1786b8dafa96ae6ab3
shawnlawyer <shawnlawyer@gmail.com>
2016-02-22 19:09:59 -0500

    moo

--- src/Application/Modules/Site/Routes/XHR/Cards.class.php
+++ src/Application/Modules/Site/Routes/XHR/Cards.class.php
@@ -12,14 +12,18 @@ class Cards {
     public static $module = Module::class;
     
     public static function sequode($dom_id = 'CardsContainer'){
+        
         $js = array();
         $deck = array();
-        $deck[] = ModuleCard::render(Sequode\Application\Modules\Site\Module::$registry_key, 'sequode', true, true, true);
+        $deck[] = ModuleCard::render(\Sequode\Application\Modules\Site\Module::$registry_key, 'sequode', true, true, true);
         $js[] = CardKitJS::placeDeck($deck, $dom_id);
         $deck = array();
-        $deck[] = ModuleCard::render(Sequode\Application\Modules\Auth\Module::$registry_key, 'login');
-        $deck[] = ModuleCard::render(Sequode\Application\Modules\Registrt\Module::$registry_key, 'signup');
+        $deck[] = ModuleCard::render(\Sequode\Application\Modules\Auth\Module::$registry_key, 'login');
+        $deck[] = ModuleCard::render(\Sequode\Application\Modules\Registry\Module::$registry_key, 'signup');
         $js[] = CardKitJS::placeDeck($deck, $dom_id, false);
-        return implode('',$js);
+        
+        return implode('', $js);
+        
     }
+    
 }
