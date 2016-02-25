3f226ceb3945819a3e09c2641c4df7cae55491fd
shawnlawyer <shawnlawyer@gmail.com>
2016-02-24 23:17:58 -0500

    moo

--- src/Application/Modules/Account/Components/Cards.class.php
+++ src/Application/Modules/Account/Components/Cards.class.php
@@ -64,39 +64,64 @@ class Cards {
         $dialogs = $module::model()->components->dialogs;
         $dialog = forward_static_call_array(array($dialogs, __FUNCTION__), array());
         
+        if(!SessionStore::is($dialog->session_store_key)){
+            SessionStore::set($dialog->session_store_key, $dialog->session_store_setup);
+        }
+        
+        $dialog_store = SessionStore::get($dialog->session_store_key);
         $step = $dialog->steps[$dialog_store->step];
+        
         $_o = (object) null;
         $_o->icon_background = 'users-icon-background';
         $_o->size = 'small';
+        
         if($dialog_store->step != 0){
+            
             $_o->menu = (object) null;
             $_o->menu->items = array();
             $_o->menu->items[] = CardKit::onTapEventsXHRCallMenuItem('Start Over','operations/account/' . __FUNCTION__,array(FormComponent::jsQuotedValue('{"reset":"1"}')));
+        
         }
+        
         $_o->head = 'Account Password';
         $_o->body = array('');
+        
         if(isset($step->content)){
+            
             if(isset($step->content->head)){
+                
                 $_o->body[] = '<div class="subline">'.$step->content->head.'</div>';
+                
             }
+            
             if(isset($step->content->head)){
+                
                 $_o->body[] = $step->content->body;
+                
             }
+            
         }
         
         if(isset($step->forms)){
+            
             foreach($step->forms as $form){
+                
                 $_o->body = array_merge($_o->body, ModuleForm::render($module::$registry_key, $form));
+                
             }
+            
         }
         
         if($dialog_store->step != 0){
+            
             $_o->body[] = CardKit::resetDialogButton('operations/account/' . __FUNCTION__);
+            
         }
         
         $_o->body[] = (object) array('js' => '$(\'.focus-input\').focus(); $(\'.focus-input\').select();');
         
-        return $_o;    
+        return $_o;
+        
     }
     
     public static function updateEmail(){
@@ -104,6 +129,11 @@ class Cards {
         $module = static::$module;
         $dialogs = $module::model()->components->dialogs;
         $dialog = forward_static_call_array(array($dialogs, __FUNCTION__), array());
+        
+        
+        if(!SessionStore::is($dialog->session_store_key)){
+            SessionStore::set($dialog->session_store_key, $dialog->session_store_setup);
+        }
         
         $dialog_store = SessionStore::get($dialog->session_store_key);
         $step = $dialog->steps[$dialog_store->step];
