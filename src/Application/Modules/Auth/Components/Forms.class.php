2b617e7fff632efccd0975486c9e8cefbe3a8fb3
shawnlawyer <shawnlawyer@gmail.com>
2016-02-26 01:12:14 -0500

    moo

--- src/Application/Modules/Auth/Components/Forms.class.php
+++ src/Application/Modules/Auth/Components/Forms.class.php
@@ -6,22 +6,36 @@ use Sequode\Component\Form\Form as FormComponent;
 
 use Sequode\Application\Modules\Auth\Module;
 
-class Forms   {
+class Forms {
     
     public static $module = Module::class;
     
-	public static $objects_source = FormInputs::class;
-	public static $xhr_library = 'operations/auth';
 	public static function login(){
-        $_o = FormComponent::formObject(static::$objects_source, __FUNCTION__, static::$xhr_library, func_get_args());
-        $_o->submit_xhr_call_route = static::$xhr_library.'/'.'login';
+        
+        $module = static::$module;
+        $context = $module::model()->context;
+        $form_inputs = $module::model()->components->form_inputs;
+        
+        $_o = FormComponent::formObject2($form_inputs, __FUNCTION__, func_get_args());
+        $_o->submit_xhr_call_route = FormComponent::xhrCallRoute($context, 'operations', 'login');
         $_o->submit_button = 'Next';
+        
 		return $_o;
+        
 	}
+    
     public static function secret(){
-        $_o = FormComponent::formObject(static::$objects_source, __FUNCTION__, static::$xhr_library, func_get_args());
-        $_o->submit_xhr_call_route = static::$xhr_library.'/'.'login';
+        
+        $module = static::$module;
+        $context = $module::model()->context;
+        $form_inputs = $module::model()->components->form_inputs;
+        
+        $_o = FormComponent::formObject2($form_inputs, __FUNCTION__, func_get_args());
+        $_o->submit_xhr_call_route = FormComponent::xhrCallRoute($context, 'operations', 'login');
         $_o->submit_button = 'Next';
+        
 		return $_o;
+        
 	}
+    
 }
--- src/Application/Modules/Package/Components/Forms.class.php
+++ src/Application/Modules/Package/Components/Forms.class.php
@@ -4,30 +4,57 @@ namespace Sequode\Application\Modules\Package\Components;
 
 use Sequode\Component\Form\Form as FormComponent;
 
-class Forms   {
-    public static $objects_source = FormInputs::class;
-	public static $xhr_library = 'operations/package';
+use Sequode\Application\Modules\Auth\Module;
+
+class Forms {
+    
     public static function name($_model = null){
-		$_o = FormComponent::formObject(static::$objects_source, __FUNCTION__, static::$xhr_library, func_get_args());
+        
+        $module = static::$module;
+        $context = $module::model()->context;
+        $form_inputs = $module::model()->components->form_inputs;
+        
+        $_o = FormComponent::formObject2($form_inputs, __FUNCTION__, func_get_args());
+        $_o->submit_xhr_call_route = FormComponent::xhrCallRoute($context, 'operations', 'updateName');
+        
         $_o->auto_submit_time = 2000;
         $_o->submit_xhr_call_parameters = array();
-        $_o->submit_xhr_call_route = static::$xhr_library.'/'.'updateName';
         $_o->submit_xhr_call_parameters[] = \Sequode\Application\Modules\Package\Modeler::model()->id;
         $_o->submit_xhr_call_parameters[] = FormComponent::$collection_replacement_hook;
+        
 		return $_o;
+        
 	}
+    
     public static function packageSequode($_model = null){
-		$_o = FormComponent::formObject(static::$objects_source, __FUNCTION__, static::$xhr_library, func_get_args());
+        
+        $module = static::$module;
+        $context = $module::model()->context;
+        $form_inputs = $module::model()->components->form_inputs;
+        
+        $_o = FormComponent::formObject2($form_inputs, __FUNCTION__, func_get_args());
+        $_o->submit_xhr_call_route = FormComponent::xhrCallRoute($context, 'operations', 'updatePackageSequode');
+        
         $_o->auto_submit_time = 1;
         $_o->submit_xhr_call_parameters = array();
-        $_o->submit_xhr_call_route = static::$xhr_library.'/'.'updatePackageSequode';
         $_o->submit_xhr_call_parameters[] = \Sequode\Application\Modules\Package\Modeler::model()->id;
         $_o->submit_xhr_call_parameters[] = FormComponent::$collection_replacement_hook;
+        
 		return $_o;
+        
 	}
+    
     public static function search(){
-        $_o = FormComponent::formObject(static::$objects_source, __FUNCTION__, static::$xhr_library, func_get_args());
+        
+        $module = static::$module;
+        $context = $module::model()->context;
+        $form_inputs = $module::model()->components->form_inputs;
+        
+        $_o = FormComponent::formObject2($form_inputs, __FUNCTION__, func_get_args());
+        $_o->submit_xhr_call_route = FormComponent::xhrCallRoute($context, 'operations', 'search');
         $_o->auto_submit_time = 1;
+        
 		return $_o;
+        
 	}
 }
