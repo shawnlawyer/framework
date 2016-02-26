683c16f6e8b9a1bd55dba94b94e4d00627b8f86c
shawnlawyer <shawnlawyer@gmail.com>
2016-02-26 15:37:19 -0500

    moo

--- src/Application/Modules/User/Traits/Operations/ORMModelSetRoleTrait.class.php
+++ src/Application/Modules/User/Traits/Operations/ORMModelSetRoleTrait.class.php
@@ -5,15 +5,18 @@ namespace Sequode\Application\Modules\User\Traits\Operations;
 trait ORMModelSetRoleTrait {
     
     public static function updateRole($role_model = null, $_model = null){
+        
         $modeler = static::$modeler;
         
-        if($role_model == null ){ $role_model = \Sequode\Application\Modules\Role\Modeler::model(); }
+        if($role_model == null ){
+            $role_model = \Sequode\Application\Modules\Role\Modeler::model();
+        }
         
         ($_model == null)
-            ? forward_static_call_array(array($modeler,'model'),array())
-            : forward_static_call_array(array($modeler,'model'),array($_model)) ;
+            ? forward_static_call_array(array($modeler, 'model'), array())
+            : forward_static_call_array(array($modeler, 'model'), array($_model)) ;
             
-        $modeler::model()->updateField($role_model->id, 'role_id')
+        $modeler::model()->updateField($role_model->id, 'role_id');
         
         return $modeler::model();
     }
