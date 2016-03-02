3c61189ab5bd88970d89b113364d82e362dbbd04
shawnlawyer <shawnlawyer@gmail.com>
2016-03-02 02:07:47 -0500

    moops

--- src/Application/Modules/FormInput/Modeler.class.php
+++ src/Application/Modules/FormInput/Modeler.class.php
@@ -2,7 +2,7 @@
 
 namespace Sequode\Application\Modules\FormInput;
 
-use Sequode\Application\Modules\User\Model as Model;
+use Sequode\Application\Modules\FormInput\Model as Model;
 
 class Modeler extends \Sequode\Patterns\Modeler {
 	public static $model = Model::class;
--- src/Application/Modules/Sequode/Traits/Operations/ORMModelSetFormInputComponentSettingsTrait.class.php
+++ src/Application/Modules/Sequode/Traits/Operations/ORMModelSetFormInputComponentSettingsTrait.class.php
@@ -25,7 +25,7 @@ trait ORMModelSetFormInputComponentSettingsTrait {
         }
         
 		$form_object = json_decode($modeler::model()->$object_member);
-		$component = new \Sequode\Application\Modules\FormInputs\Model;
+		$component = new \Sequode\Application\Modules\FormInput\Model;
 		if(!$component->exists($input_object->Component,'name')){
             
             $component->exists('str','name');
