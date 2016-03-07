427af26512d2c4c1182877f69b9a6b422ba73d1b
shawnlawyer <shawnlawyer@gmail.com>
2016-03-06 23:29:05 -0500

    moo

--- src/Application/Modules/Auth/Routes/XHR/Operations.class.php
+++ src/Application/Modules/Auth/Routes/XHR/Operations.class.php
@@ -85,9 +85,6 @@ class Operations {
             SessionStore::set($dialog->session_store_key, $dialog_store);
             
             $console_module = ModuleRegistry::model()['Console'];
-            
-            echo 'alert(\''.$console_module::$registry_key.'\');';
-            
             return (intval($dialog_store->step) == 2)
         ? forward_static_call_array(array($console_module::model()->routes['http'], 'js'), array(false))
                 : forward_static_call_array(array($xhr_cards, __FUNCTION__), array());
