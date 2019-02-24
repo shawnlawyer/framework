fa90c332b193cc27f91d8c83c99af629282c2d06
Shawn Lawyer <shawnlawyer@gmail.com>
2019-02-24 16:13:58 -0500

    fixed sequode packace class exporter, needed to check for instance of global stdClass ( \stdClass replaces stdClass)

--- src/Sequode/View/Export/PHPClosure.php
+++ src/Sequode/View/Export/PHPClosure.php
@@ -3,18 +3,18 @@ namespace Sequode\View\Export;
 
 class PHPClosure {
     public static function export($variable, $return = false, $delimeter_nl = true ) {
-        if ($variable instanceof stdClass) {
+        if ($variable instanceof \stdClass) {
             $result = '(object) '.self::export(get_object_vars($variable), true, false);
         } else if (is_array($variable)) {
-            $array = array ();
+            $array = [];
             foreach ($variable as $key => $value) {
                 $array[] = var_export($key, true).' => '.self::export($value, true);
             }
             if($delimeter_nl == true){
-            $result = 'array ('.implode(',
-        ', $array).')';
+            $result = '['.implode(',
+        ', $array).']';
             }else{
-            $result = 'array ('.implode(', ', $array).')';
+            $result = '['.implode(', ', $array).']';
             }
         } else {
             $result = var_export($variable, true);
