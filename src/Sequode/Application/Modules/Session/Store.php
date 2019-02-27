74889a9702115fc54640cfb1898e4a18346f9ab2
Shawn Lawyer <shawnlawyer@gmail.com>
2019-02-27 01:51:07 -0500

    just adding some notes here about the future

--- src/Sequode/Application/Modules/Session/Store.php
+++ src/Sequode/Application/Modules/Session/Store.php
@@ -10,7 +10,13 @@ class Store {
     
     use StaticStoreTrait,
         IsSetGetClearSessionStore,
+        /*
+         *  with ActiveModelTrait, i think i was trying to use a static storage like a session and
+         * then attach a model that it might be saved to a persistant model in the db in stead of
+         * having to loosly use them together with no logically awareness other then what's hard coded.
+         *
+         * the trait's method's available to this class are not used.
+         * */
         ActiveModelTrait;
-        
     public static $operations = Operations::class;
 }
