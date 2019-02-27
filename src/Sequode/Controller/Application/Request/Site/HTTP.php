aec67cdcbf82019f1b66db5a60d729549320160a
Shawn Lawyer <shawnlawyer@gmail.com>
2019-02-27 06:05:37 -0500

    factor: renamed Routeable to Routeables and changed dir and namespace

--- src/Sequode/Controller/Application/Request/Site/HTTP.php
+++ src/Sequode/Controller/Application/Request/Site/HTTP.php
@@ -3,7 +3,7 @@
 namespace Sequode\Controller\Application\Request\Site;
 
 use Sequode\Model\Application\Configuration;
-use Sequode\Model\Application\Routeable;
+use Sequode\Model\Application\Module\Routeables;
 use Sequode\Model\Application\Routes;
 use Sequode\Controller\Application\Request\Traits\RequestURIPiecesTrait;
 use Sequode\Controller\Application\Request\Traits\RequestCallTrait;
@@ -16,7 +16,7 @@ class HTTP {
 	public static function handle(){
         
 		$route_class = false;
-        $routeables = Routeable::model()->routes;
+        $routeables = Routeables::model()->routes;
 		$route = 'index';
 
         $request_pieces = static::URIPieces();
--- src/Sequode/Model/Application/Module/Routeables.php
+++ src/Sequode/Model/Application/Module/Routeables.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace Sequode\Model\Application;
+namespace Sequode\Model\Application\Module;
 
 use Sequode\Model\Module\Registry as ModuleRegistry;
 
-class Routeable
+class Routeables
 {
     public static function model()
     {
@@ -59,5 +59,23 @@ class Routeable
             }
         }
         return $_o;
+    }
+
+    public static function routes($routes_class){
+        if(property_exists($routes_class, 'routes') && is_array($routes_class::$routes)){
+            if(isset($routes_class::$merge) && $routes_class::$merge == true){
+                return array_merge(get_class_methods($routes_class),$routes_class::$routes);
+            }
+            return $routes_class::$routes;
+        }
+        return get_class_methods($routes_class);
+    }
+
+    public static function route($routes_class, $route){
+        if(isset($routes_class::$routes_to_methods)	&& is_array($routes_class::$routes_to_methods) && array_key_exists($route,$routes_class::$routes_to_methods)){
+            $routes = $routes_class::$routes_to_methods;
+            return $routes[$route];
+        }
+        return $route;
     }
 }
